42edcabbcb5a1325880c1d1941df9991c6895a07
ishwo6375 <ishaan.prazapati6375@gmail.com>
2021-11-22 20:17:15 -0500

    DSA pratice recursion problem recursiveRange

---
+++ Algo-Udemy/Recursion/Recursive-range.js
@@ -0,0 +1,14 @@
+//Write a function called recursiveRange which accepts a number and adds up all the 
+//numbers from 0 to the number passed to the function//
+
+// SAMPLE INPUT/OUTPUT
+// recursiveRange(6) // 21
+// recursiveRange(10) // 55 
+
+function recursiveRange(x){
+   if (x === 0 ) return 0;
+   return x + recursiveRange(x-1);
+}
+
+recursiveRange(6) // output: 21 // 1 * 2 * 3 * 4 * 5 * 6
+
