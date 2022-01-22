4fa9dde06567111e57a69cfd2d3fd54b8a0936e0
ishwo6375 <ishaan.prazapati6375@gmail.com>
2022-01-22 16:08:02 -0500

    refactored code in charCount problem

--- Algo-Udemy/patterns/charCount.js
+++ Algo-Udemy/patterns/charCount.js
@@ -14,10 +14,8 @@ function charCount(str){
 
   for(let char of str){
     char = char.toLowerCase();
-    if(obj[char] > 0){
-      obj[char]++;
-    }else{
-      obj[char] = 1;
+    if(/[a-z0-9]/.test(char)){
+      obj[char] = ++obj[char] || 1;
     }
   }
   return obj;
