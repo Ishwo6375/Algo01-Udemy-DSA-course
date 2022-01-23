bc0d5f07f3a892e03726fc21a4a8a8e6018e10bd
ishwo6375 <ishaan.prazapati6375@gmail.com>
2022-01-22 20:39:00 -0500

    added psuedo code to countUniqueValues.js

--- Algo-Udemy/patterns/countUniqueValues.js
+++ Algo-Udemy/patterns/countUniqueValues.js
@@ -1,7 +1,29 @@
+//Count the index of unique values in the array//
+
+//examples//
+// input = [1,2,2,3,4,4,5,6,6]
+// output = [6] => here we have 6 unique values without counting dublicates//
+ //example 2 = [2,3,3,4,4,5,5,6,7,8,9]//
+ //output = [8] => we have 8 unique values here//
+
+ //psuedo code//
+
+ //soution using two pointer//
+ //set first pointer equals to 1st index of array//
+ //loop over each elements in the array//
+ //set another pointer equals to index of 1 of array//
+ //check condition if value of first pointer is equal to value of second pointer or not//
+ //if it is not equal increment first pointer by 1//
+ //set value of second pointer equals to first pointer//
+ // retrun index of array//
+
+
+
+
 function countUniqueValues(arr){
     if(arr.length === 0) return 0;
     var i = 0;
-    for(var j = 0; j < arr.length; j++){
+    for(var j = 1; j < arr.length; j++){
         if(arr[i] !== arr[j]){
             i++;
             arr[i] = arr[j];
